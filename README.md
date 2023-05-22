3d877ed03d55fcdf8d55b5493a59466ac1af29e1
Teea Yu Xuan <yuxuanteea02@gmail.com>
2023-05-23 00:11:43 +0800

    Create rules and inference engine for Common Injury page in Injury tab (not complete yet)

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 Building an Expert System on Python with Streamlit
 
 # How to run
-1. Go to Terminal and install required libraries using this command <pip install -r requirements.txt>
-2. In Terminal, use this command <streamlit run "main.py"> to run
+1. Use Python 3.8 or below to run (Download Python 3.8 here: https://www.python.org/ftp/python/3.8.0/python-3.8.0-amd64.exe and edit the PATH in Environment Variables by following this guide: https://www.youtube.com/watch?v=lezhrFdVSVY)
+2. Go to Terminal and install required libraries using this command <pip install -r requirements.txt>
+3. In Terminal, use this command <streamlit run "main.py"> to run
 
 
--- apps/Injury.py
+++ apps/Injury.py
@@ -1,8 +1,22 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+from experta import Rule, Fact, KnowledgeEngine
+
+class InjuryType(Fact):
+    """Info about the injury type"""
+    pass
+
+class CommonInjury(KnowledgeEngine):
+    @Rule(InjuryType(injury_type='Sprain'))
+    def sprain(self):
+        st.write("Sprain")
+
+    @Rule(InjuryType(injury_type='Strain'))
+    def strain(self):
+        st.write("Strain")
+
 
 def app():
-
     with st.sidebar:
         selected = option_menu(
             menu_title=None,
@@ -12,7 +26,16 @@ def app():
         )
 
     if selected == "Common Injury":
-        st.write("Common Injury")
+        st.markdown("""<h2 style="text-align: center;">Common Injury</h2>""", unsafe_allow_html=True)
+
+        st.markdown("""<h4>Select the type of injury</h4>""", unsafe_allow_html=True)
+        injury = st.selectbox("", ["Sprain", "Strain"])
+
+        engine = CommonInjury()
+        engine.reset()
+        engine.declare(InjuryType(injury_type=injury))
+        engine.run()
+
     elif selected == "Treatment":
         st.write("Treatment")
     elif selected == "Prevention":
